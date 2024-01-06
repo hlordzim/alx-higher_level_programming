3b91010ab94f9f6c1b29895fe8bfa6010d1bf5a2
hlordzim <hlordzim.mh@gmail.com>
2024-01-06 09:09:03 -0800

    8. ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
-
 def magic_calculation(a, b):
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
+
     if a < b:
         c = add(a, b)
-        for i in range(4, 6)
-        c = add(a, i)
+        for i in range(4, 6):
+            c = add(c, i)
         return (c)
-    else
-    return sub(a, b)
+    else:
+        return sub(a, b)
