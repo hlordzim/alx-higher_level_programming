4fbe9c3ded3a342877cc823ba3327c7050e56128
hlordzim <hlordzim.mh@gmail.com>
2024-01-23 15:10:43 -0800

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,14 +3,14 @@ class Square:
     """
     Square class represents a square.
 
-    This class is currently empty and serves as a placeholder for future implementation.
+    Empty class and serves as a placeholder for future implementation.
     """
 
     def __init__(self):
         """
         Initializes a new instance of the Square class.
 
-        This method is empty as there are no attributes to initialize at the moment.
+        Empty Method as no attributes to initialize at the moment.
         """
         pass
 
@@ -18,6 +18,6 @@ class Square:
         """
         Placeholder for a future method.
 
-        This method is currently empty and serves as a placeholder for future implementation.
+        Empty Method and serves as a placeholder for future implementation.
         """
         pass
