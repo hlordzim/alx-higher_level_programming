ba6ac94496eb46cc0237dde92dd3021f2341d888
hlordzim <hlordzim.mh@gmail.com>
2024-02-05 16:33:12 -0800

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines an inherited list Mylist"""
+
+class MyList(list):
+    """Inherits from list and provides additional method print_sorted."""
+
+    def print_sorted(self):
+        """Print the list in sorted order (ascending)."""
+        sorted_list = sorted(self)
+        print(sorted_list)
