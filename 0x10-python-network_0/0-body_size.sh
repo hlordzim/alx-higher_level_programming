a262d547051ab0a3ceb77aee3753b55f1e03ed90
Michael Hlordzi <hlordzim.mh@gmail.com>
2024-04-25 09:16:39 -0700

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and 
-# displays the size of the body of the response
+#   displays the size of the body of the response
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
+
 URL=$1
 SIZE=$(curl -sI $URL | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-echo "Size of the body in bytes: $SIZE"
+echo "$SIZE"
