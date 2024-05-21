f251c843866dd919f2806e30f9e82220db21bc00
hlordzim <hlordzim.mh@gmail.com>
2024-05-21 21:20:04 +0000

    to write a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+"""script that writes a string to a file."""
+
+const fs = require('fs');
+
+// Get command line arguments
+const args = process.argv.slice(2);
+const filePath = args[0];
+const content = args[1];
+
+if (!filePath || !content) {
+    console.log("Usage: node script.js <file_path> <content>");
+    process.exit(1);
+}
+
+fs.writeFile(filePath, content, { encoding: 'utf8' }, (err) => {
+    if (err) {
+        console.error(`An error occurred: ${err}`);
+        process.exit(1);
+    } else {
+        console.log(`Content written to ${filePath} successfully.`);
+    }
+});
