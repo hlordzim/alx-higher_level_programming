a54cd06e4cce1a34a06098aa45aae95c688774ad
hlordzim <hlordzim.mh@gmail.com>
2024-05-30 13:09:40 -0700

    task_1_With JQuery

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Change Header Color</title>
-    <!-- Include jQuery from CDN -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!-- Include the script to change the header color -->
-    <script src="change_header_color.js"></script>
-</head>
-<body>
-    <header>This is the header</header>
-</body>
-</html>
+#!/usr/bin/node
+// Changes color of first header to red using JQuery
+
+$('document').ready(() => {
+  $('header').css('color', '#FF0000');
+});
