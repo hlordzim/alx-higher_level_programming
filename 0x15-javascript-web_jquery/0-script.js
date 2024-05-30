eb115bc54a1734e82094fde57006b720a678c234
hlordzim <hlordzim.mh@gmail.com>
2024-05-30 12:23:26 -0700

    task 1_no_jQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Header Color</title>
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            const header = document.querySelector('header');
+            header.style.color = '#FF0000';
+        });
+    </script>
+</head>
+<body>
+    <header>This is the header</header>
+</body>
+</html>
