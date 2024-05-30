b741dfb4fac8107f435c33bc6d8926151b34e553
hlordzim <hlordzim.mh@gmail.com>
2024-05-30 13:05:33 -0700

    task_1_No JQuery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Change Header Color</title>
-    <script>
-        document.addEventListener('DOMContentLoaded', () => {
-            const header = document.querySelector('header');
-            header.style.color = '#FF0000';
-        });
-    </script>
-</head>
-<body>
-    <header>This is the header</header>
-</body>
-</html>
+#!/usr/bin/node
+// Changes the color of first header tag to red using valina JS
+
+document.querySelector('header').style.color = '#FF0000';
---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Header Color</title>
+    <!-- Include jQuery from CDN -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Include the script to change the header color -->
+    <script src="change_header_color.js"></script>
+</head>
+<body>
+    <header>This is the header</header>
+</body>
+</html>
